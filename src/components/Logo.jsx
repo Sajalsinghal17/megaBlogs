1d29794bd7c7ec8ce9a7ff0079e5cc39bbc08fc8
Sajal Singhal <sajalsinghal172005@gmail.com>
2025-11-02 18:05:33 +0530

    Add Logo image and update Header component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
-function Logo({width = '100px'}) {
+  
+function Logo({width = '200px'}) {
   return (
-    <div>Logo</div>
+    <img src="/Logo.png" alt="Logo" style={{ width}} />
   )
 }
 
